49b1ca5705af13fd431d9b8bb5c5207d081e59d9
Raed Marouf <raeed20@windowslive.com>
2014-09-20 23:36:08 +0300

    my solution
    this file contains two functions:
    ## the first function creates a litst contatining a function to:
    ## set the matrix entries, get them, set the inverse of a matrix, and
    get the it
    
    ## the second function returns the inverse of a matrix that was created
    using the first function
    ## if the inverse was calculated in prior to the call, it returns the
    cached inverted matrix
    ## if not, it calculates it and sets it in the cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,36 @@
-## [Put comments here that describe what your functions do]
+## the first function creates a litst contatining a function to: 
+## set the matrix entries, get them, set the inverse of a matrix, and get the it
+
+## the second function returns the inverse of a matrix that was created using the first function
+## if the inverse was calculated in prior to the call, it returns the cached inverted matrix
+## if not, it calculates it and sets it in the cache
+
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
